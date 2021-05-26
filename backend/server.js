b950de2be2a9a0546a3118a086b0bd049cc2ad9b
IDStack <idstack@MacBook-Pro-IDStack.local>
2021-05-26 22:05:58 +0700

    use dotenv config

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
 
+dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
@@ -16,4 +18,11 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log(`server is running on http://localhost:5000`))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `server is running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`
+  )
+)
