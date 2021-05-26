f851c21934eb33fecccb37f5377b97bb61d4e0c3
lionatic <ahmadhakim404@gmail.com>
2021-05-26 12:15:17 +0700

    create header footer components with react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.css'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className='App'>
-      <h1>IDShop</h1>
-    </div>
+    <>
+      <Header></Header>
+      <main>
+        <Container>
+          <h1>Welcome to IDShop</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </>
   )
 }
 
