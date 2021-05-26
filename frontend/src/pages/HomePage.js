941409bc1319a74e3e6dc72d9e0915a36ecddb32
IDStack <idstack@MacBook-Pro-IDStack.local>
2021-05-26 21:43:34 +0700

    fetch data products from server

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
-import products from '../products'
 import Product from '../components/Product'
+import axios from 'axios'
 
 const HomePage = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [])
+
   return (
     <>
       <h1>Latests Products</h1>
