5c6bec0833852081cade8645dfa27da674e7b003
lionatic <ahmadhakim404@gmail.com>
2021-05-26 16:31:02 +0700

    implement react-router for spa

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,28 @@
-import { Container, Navbar, Nav } from 'react-bootstrap'
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='#home'>IDShop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>IDShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='#home'>
-                <i className='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-              <Nav.Link href='#link'>
-                <i className='fas fa-user'></i> Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
