613a303c6444501d42e1a14fe33c89c4a233313b
IDStack <idstack@MacBook-Pro-IDStack.local>
2021-05-27 22:10:51 +0700

    make seeder script for import data

--- backend/data/users.js
+++ backend/data/users.js
@@ -3,19 +3,19 @@ import bcrypt from 'bcryptjs'
 const users = [
   {
     name: 'Admin',
-    emai: 'admin@mail.com',
+    email: 'admin@mail.com',
     password: bcrypt.hashSync('password', 10),
     isAdmin: true,
   },
   {
     name: 'User',
-    emai: 'user@mail.com',
+    email: 'user@mail.com',
     password: bcrypt.hashSync('password', 10),
     isAdmin: false,
   },
   {
     name: 'Hakim',
-    emai: 'hakim@mail.com',
+    email: 'hakim@mail.com',
     password: bcrypt.hashSync('password', 10),
     isAdmin: false,
   },
