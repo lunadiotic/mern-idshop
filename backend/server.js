17519c8b45383c924134b9262b1af055f1246513
IDStack <idstack@MacBook-Pro-IDStack.local>
2021-05-30 20:32:57 +0700

    fetch product from mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+// import products from './data/products.js'
+
+import productRoute from './routes/product.route.js'
 
 dotenv.config()
 
@@ -14,14 +16,7 @@ app.get('/', (req, res) => {
   res.send('server is running')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/api/products', productRoute)
 
 const PORT = process.env.PORT || 5000
 
